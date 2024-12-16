1d251beb558ef63972bb704b57c18afc0f79ddfe
fzft <578847278@qq.com>
2024-12-16 18:44:28 +0800

    mask fill

--- llm-impl/attention/src/lib.rs
+++ llm-impl/attention/src/lib.rs
@@ -1,9 +1,6 @@
-use candle_core::{Tensor, Device,Result};
+use candle_core::{Device, Result, Tensor, D};
 use candle_nn::ops::softmax;
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use candle_nn::{Linear, Module};
 
 pub fn naive_softmax(x: &Tensor) -> Tensor {
     let exp = x.exp().unwrap();
@@ -13,26 +10,42 @@ pub fn naive_softmax(x: &Tensor) -> Tensor {
     exp.broadcast_div(&sum).unwrap()
 }
 
+pub fn masked_fill(on_false: &Tensor, mask: &Tensor, on_true: f32) -> Result<Tensor> {
+    let shape = mask.shape();
+    let on_true = Tensor::new(on_true, on_false.device())?.broadcast_as(shape.dims())?;
+    println!("{}", on_true);
+    println!("{}", on_false);
+    println!("{}", mask);
+    let m = mask.where_cond(&on_true, &on_false)?;
+    Ok(m)
+}
+
 pub struct SelfAttentionV1 {
     w_query: Tensor,
     w_key: Tensor,
     w_value: Tensor,
-    device: Device
+    device: Device,
 }
 
 impl SelfAttentionV1 {
     pub fn new(d_in: usize, d_out: usize) -> Self {
         let device = Device::Cpu;
         let w_query = Tensor::rand(0.0, 1.0, (d_in, d_out), &device).unwrap();
-        let w_key = Tensor::rand(0.0, 1.0, (    d_in, d_out), &device).unwrap();
+        let w_key = Tensor::rand(0.0, 1.0, (d_in, d_out), &device).unwrap();
         let w_value = Tensor::rand(0.0, 1.0, (d_in, d_out), &device).unwrap();
-        Self {device, w_query, w_key, w_value }
+        Self {
+            device,
+            w_query,
+            w_key,
+            w_value,
+        }
     }
 
     pub fn forward(&self, x: &Tensor) -> Result<Tensor> {
-        // (T, d_in) -> (T, d_out) 
+        // (T, d_in) -> (T, d_out)
 
         // Project input into query, key and value vectors
+        // (T, d_in) * (d_in, d_out) -> (T, d_out)
         let queries = self.project_queries(x)?;
         let keys = self.project_keys(x)?;
         let values = self.project_values(x)?;
@@ -42,7 +55,7 @@ impl SelfAttentionV1 {
         let scaled_scores = self.scale_scores(attn_scores)?;
 
         // Apply softmax to get attention weights and compute final context
-        let attn_weights = softmax(&scaled_scores, 1)?;
+        let attn_weights = softmax(&scaled_scores, D::Minus1)?;
         let context = attn_weights.matmul(&values)?;
 
         Ok(context)
@@ -69,7 +82,46 @@ impl SelfAttentionV1 {
         let scale_factor = Tensor::new(vec![dk as f64], &self.device)?.sqrt()?;
         scores.broadcast_div(&scale_factor)
     }
+}
 
+pub struct SelfAttentionV2 {
+    w_query: Linear,
+    w_key: Linear,
+    w_value: Linear,
+    device: Device,
+}
+
+impl SelfAttentionV2 {
+    pub fn new(d_in: usize, d_out: usize, bias: Option<Tensor>) -> Self {
+        let device = Device::Cpu;
+        let w_tensor = Tensor::rand(0.0, 1.0, (d_out, d_in), &device).unwrap();
+        let w_query = Linear::new(w_tensor.clone(), bias.clone());
+        let w_key = Linear::new(w_tensor.clone(), bias.clone());
+        let w_value = Linear::new(w_tensor.clone(), bias.clone());
+        Self {
+            device,
+            w_query,
+            w_key,
+            w_value,
+        }
+    }
+
+    pub fn forward(&self, x: &Tensor) -> Result<Tensor> {
+        let queries = self.w_query.forward(x)?;
+        let keys = self.w_key.forward(x)?;
+        let values = self.w_value.forward(x)?;
+        let attn_scores = queries.matmul(&keys.t()?)?;
+        let scaled_scores = self.scale_scores(attn_scores)?;
+        let attn_weights = softmax(&scaled_scores, D::Minus1)?;
+        let context = attn_weights.matmul(&values)?;
+        Ok(context)
+    }
+
+    fn scale_scores(&self, scores: Tensor) -> Result<Tensor> {
+        let (_, dk) = scores.shape().dims2()?;
+        let scale_factor = Tensor::new(vec![dk as f64], &self.device)?.sqrt()?;
+        scores.broadcast_div(&scale_factor)
+    }
 }
 
 #[cfg(test)]
@@ -122,7 +174,6 @@ mod tests {
         println!("{:?}", context_vec_2);
     }
 
-
     #[test]
     fn it_works_2() {
         let device = Device::Cpu;
@@ -144,7 +195,12 @@ mod tests {
             for j in 0..t.shape().dims()[0] {
                 let x_i = t.get(i).unwrap();
                 let x_j = t.get(j).unwrap();
-                let score = (&x_i * &x_j).unwrap().sum_all().unwrap().to_scalar::<f64>().unwrap();
+                let score = (&x_i * &x_j)
+                    .unwrap()
+                    .sum_all()
+                    .unwrap()
+                    .to_scalar::<f64>()
+                    .unwrap();
                 row_scores.push(score);
             }
             attn_scores.push(row_scores);
@@ -170,7 +226,7 @@ mod tests {
         .unwrap();
         let attn_scores = t.matmul(&t.t().unwrap()).unwrap();
         let softmax_scores = softmax(&attn_scores, 1).unwrap();
-        let all_context_vecs =  softmax_scores.matmul(&t).unwrap();
+        let all_context_vecs = softmax_scores.matmul(&t).unwrap();
         println!("{}", all_context_vecs)
     }
 
@@ -195,6 +251,43 @@ mod tests {
         let self_attn = SelfAttentionV1::new(d_in, d_out);
         let out = self_attn.forward(&t).unwrap();
         println!("{}", out);
+    }
 
+    #[test]
+    fn test_self_attention_v2() {
+        let device = Device::Cpu;
+        let t = Tensor::new(
+            vec![
+                vec![0.43, 0.15, 0.89], // Your     (x^1)
+                vec![0.55, 0.87, 0.66], // journey  (x^2)
+                vec![0.57, 0.85, 0.64], // starts   (x^3)
+                vec![0.22, 0.58, 0.33], // with     (x^4)
+                vec![0.77, 0.25, 0.10], // one      (x^5)
+                vec![0.05, 0.80, 0.55], // step     (x^6)
+            ],
+            &device,
+        )
+        .unwrap();
+        let d_in = t.shape().dims()[1];
+        let d_out = 2;
+        let self_attn = SelfAttentionV2::new(d_in, d_out, None);
+        let out = self_attn.forward(&t).unwrap();
+        println!("{}", out);
+    }
+
+    #[test]
+    fn test_tril() {
+        let device = Device::Cpu;
+        let tril = Tensor::tril2(6, DType::F64, &device).unwrap();
+        println!("{}", tril);
+    }
+
+    #[test]
+    fn test_masked_fill() {
+        let device = Device::Cpu;
+        let attn_scores = Tensor::rand(0.0, 1.0, (6, 6), &device).unwrap();
+        let tril = Tensor::tril2(6, DType::F64, &device).unwrap();
+        let masked = masked_fill(&attn_scores, &tril, f64::NEG_INFINITY).unwrap();
+        println!("{}", masked);
     }
 }
